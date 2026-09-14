0413cef9d15a8b8d03266e44c90f1e946ad92982
glo10 <contact@tshimini.fr>
2026-09-14 14:34:03 +0200

    feat: demo OS

---
+++ 3-demos/6-os.mjs
@@ -0,0 +1,18 @@
+import * as os from 'node:os';
+// Basic system information
+console.log(`OS Platform: ${os.platform()}`);
+console.log(`OS Type: ${os.type()}`);
+console.log(`OS Release: ${os.release()}`);
+console.log(`CPU Architecture: ${os.arch()}`);
+console.log(`NB CPUS`, os.cpus())
+console.log(`Hostname: ${os.hostname()}`);
+
+// Memory information
+const totalMemGB = (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2);
+const freeMemGB = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
+console.log(`Memory: ${freeMemGB}GB free of ${totalMemGB}GB`);
+
+// User information
+const userInfo = os.userInfo();
+console.log(`Current User: ${userInfo.username}`);
+console.log(`Home Directory: ${os.homedir()}`);
