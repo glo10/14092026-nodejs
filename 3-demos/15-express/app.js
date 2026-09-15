53c76c344177a5e17819b74e3f9f5465306dfcb2
glo10 <contact@tshimini.fr>
2026-09-15 14:50:34 +0200

    refactor: add more comments

--- 3-demos/15-express/app.js
+++ 3-demos/15-express/app.js
@@ -14,14 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev')); // middleware journalisation
+app.use(logger('dev')); // middleware journalisation (log)
 app.use(express.json()); // middleware qui transforme le body au format JSON en objet JS ie JSON.parse(body)
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); // parser les en-têtes HTTP pour récupérer les infos plus facile via JS
 app.use(express.static(path.join(__dirname, 'public'))); // dossier public = dossier statique /images/logo.svg
 
 app.use('/', indexRouter); 
-app.use('/users', usersRouter); // Les routes commençant par /users seront traitées par le router usersRouter
+app.use('/users', usersRouter); // Les routes commençant par /users (racine des routes /users) seront traitées par le router usersRouter
 app.use('/product', productRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- 3-demos/15-express/routes/product.js
+++ 3-demos/15-express/routes/product.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET product home page. */
 router.get("/", function (req, res, next) {
+  // Dans le modèle MVC ici c'est la partie V pour View ou Vue en fr
   res.render(
     "product/homepage", // le template qui sera views/product/homepage.ejs
-    // les données qui seront injecté et interprêté par le moteur pour générer dynamiquement le HTML en fonction des infos
+    // les données qui seront injectées et interprêtées par le moteur pour générer dynamiquement le HTML en fonction des infos
     { title: "Express", items: [ 'produit 1', 'produit 2', 'produit 3'] },
-  ); // la vue views
+  ); 
 });
 
 module.exports = router;
