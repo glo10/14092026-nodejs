3dfa2920187209710b7d080516795959d8646dd2
glo10 <contact@tshimini.fr>
2026-09-15 15:05:55 +0200

    feat: correction ex4.1

---
+++ 1-ateliers/corrections/4.1/src/utils/folders.mjs
@@ -0,0 +1,10 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+export const publicDir = join(
+  dirname(fileURLToPath(import.meta.url)),
+  '..',
+  '..',
+  'public'
+)
+export const homepage = join(publicDir, 'html', 'index.html')
